c0512b416fd1283cb14ba22d44331b3df4784861
Michael <mltor8@gmail.com>
2026-08-09 23:00:54 -0400

    Tasks 1-5 are made.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,31 +13,47 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify({books}, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Retrieve the isbn parameter from the request URL and send the corresponding book's details
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let foundByAuthor = [];
+  const keys = Object.keys(books);
+  for(const key of keys) {
+    if(books[key].author === author)
+      foundByAuthor.push(books[key]);
+  }
+
+  if(foundByAuthor.length > 0)
+    res.send(foundByAuthor);
+  else
+    res.send("There is no such author.");
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const keys = Object.keys(books);
+  for(const key of keys) {
+    if(books[key].title === title)
+      res.send(books[key]);
+  }
+  res.send("There is no such title.");
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
